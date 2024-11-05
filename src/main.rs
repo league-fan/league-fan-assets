6d6c1c2f70ddce1d20dd8d07a642dc82d2cc9cde
SnowMemo <48174882+snowmemo@users.noreply.github.com>
2024-11-05 01:42:01 +0000

    add loot download

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
-use std::{fs::File, io::Write};
+use std::{path::Path, sync::Arc};
 
 use league_fan_assets::types::preludes::*;
+use tokio::fs;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 3)]
 async fn main() {
-    let config = Config::new(None, LanguageType::ChineseChina);
+    let config = Arc::new(Config::new(None, LanguageType::ChineseChina));
 
     let loot = Loot::get(&config).await.unwrap();
     let chest = loot
         .loot_items
         .iter()
         .filter(|x| x.type_field == TypeField::Chest)
+        .cloned() // 克隆数据
         .collect::<Vec<_>>();
-    let tasks = chest.iter().map(|x| async {
-        if x.image.trim_start_matches(' ').is_empty() {
-            return;
-        }
-        let url = get_cdragon_url(x.image.as_str(), &config);
-        eprintln!("Origin: {}", x.image);
-        eprintln!("Downloading: {}", url);
-        let body = reqwest::get(&url).await.unwrap().bytes().await.unwrap();
-        let path = format!("assets/loot/{}", x.image);
-        let mut file = File::create(path).unwrap();
-        file.write_all(&body).unwrap();
-    });
 
-    for task in tasks {
-        task.await;
+    let mut handles = vec![];
+
+    for x in chest {
+        let config = Arc::clone(&config);
+        let handle = tokio::spawn(async move {
+            if x.image.trim_start_matches(' ').is_empty() {
+                return;
+            }
+            let path = Path::new(".").join(x.image.trim_start_matches('/'));
+            if fs::metadata(&path).await.is_ok() {
+                eprintln!("🔥  {:?} skiped", &path);
+                return;
+            }
+            let dir = path.parent().unwrap();
+            fs::create_dir_all(dir).await.unwrap();
+
+            let url = get_cdragon_url(x.image.as_str(), &config);
+            eprintln!("🚀  {} -> {:?}", url, &path);
+            let body = reqwest::get(&url).await.unwrap().bytes().await.unwrap();
+            fs::write(&path, &body).await.unwrap();
+        });
+        handles.push(handle);
+    }
+
+    // 等待所有任务完成
+    for handle in handles {
+        handle.await.unwrap();
     }
 }
